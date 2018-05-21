82861557630fdf575bfcd29954b8fceef9bd53d0
pierre <pierre.duval@gadz.org>
2018-05-21 18:09:58 +1000

    add axis names

--- graph.js
+++ graph.js
@@ -17,7 +17,7 @@ function plot(powerGivenSpeed) {
 
   let width = 500;
   let height = 500
-  let margin = ({top: 20, right: 30, bottom: 30, left: 40});
+  let margin = ({top: 20, right: 30, bottom: 30, left: 100});
 
   // Define the axes
   var x = d3.scaleLinear().domain(speedDomain).range([margin.left, width - margin.right]);
@@ -47,4 +47,15 @@ function plot(powerGivenSpeed) {
     .attr("stroke", "steelblue")
     .attr("stroke-width", 1.5)
     .attr("d", line);
+
+  // Add label for axis
+  svg.append("text")
+    .attr("transform", `translate(${width/2}, ${height + margin.top})`)
+    .text("Speed (km/h)");
+
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", 0 - (height / 2))
+    .attr("y", margin.left / 2)
+    .text("Power (W)");
 }
